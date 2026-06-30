71322076cb2f41721687d3a3fc8eac36653b342c
Prasad Ganesh Dongare <prasaddongare1601@gmail.com>
2026-06-30 19:30:06 +0530

    Add Docker CI/CD pipeline with GitHub Actions

--- src/main/java/com/prasad/seoinsights/SeoInsightsPlatformApplication.java
+++ src/main/java/com/prasad/seoinsights/SeoInsightsPlatformApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeoInsightsPlatformApplication {
 
 	public static void main(String[] args) {
+
+		//System.setProperty("java.net.preferIPv4Stack", "true");
 		Dotenv dotenv = Dotenv.configure()
 				.ignoreIfMissing()
 				.load();
